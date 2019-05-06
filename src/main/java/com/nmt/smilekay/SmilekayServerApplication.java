3fb72fc70cc2097b8eef459b291fb575a4725a05
nmt <419620437@qq.com>
2019-05-06 22:30:40 +0800

    add by nmt 2019/5/6 22:30

---
+++ src/main/java/com/nmt/smilekay/SmilekayServerApplication.java
@@ -0,0 +1,13 @@
+package com.nmt.smilekay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmilekayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmilekayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nmt/smilekay/SmilekayServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nmt.smilekay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmilekayServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
